6b494e624492a509c9d3c3631dbf5f47fc61f2be
cralducinr <cralducinr@CRALDUCINR-PC>
2015-09-02 18:03:35 -0500

    Mas ejercicios

---
+++ src/Genericos/Prueba.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Genericos;
+
+import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author cralducinr
+ * Esta clase es utilizada cuando Hastable rebasa un limite de capacidad
+ * trabajando con aplicaciones multiHilo. Hastable bloquea todo el mapa
+ * mientras que ConcurrentHashMap solo una parte del Mapa.
+ */
+public class Prueba {
+    
+    public static void main(String[] args) {
+        ConcurrentHashMap  c = new ConcurrentHashMap ();    
+        
+  
+        
+    }
+}
